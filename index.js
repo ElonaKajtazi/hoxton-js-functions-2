76f8fc54c58cf84aef03d4036e6d3818df209984
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-06-21 00:41:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//Instructions
+// Using the usual Users and Todo lists:
+//   - Prompt the user for a letter
+//   - Find all the users who's name contains the letter
+//   - Say hi to those users in the console - but only 1 greeting every 2 seconds.
+//   - Prompt the user for a user ID. Using the value, find all the todos that are Incomplete and console.log them.
+// Tips
+// - Don't forget to clear interval once it's done running
+// - It's perfectly okay to prompt a user for details while other stuff (i.e.the interval) is running
+// // - Keep your code separated into functions for easier readability later
+
+let enterALetter = String(prompt("Enter a letter")) 
+users.filter(function(user) {
+  let result = user.name.includes(enterALetter)
+  return result
+}
+).forEach(function(user) {
+  setInterval(function() {
+    console.log(`Hi ${user.name}`)
+  }, 2000)
+}
+)
+// Prompt the user for a user ID. Using the value, find all the todos that are Incomplete and console.log them.
+
+
+
+
+
