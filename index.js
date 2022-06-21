7efd500813a2de5ce5ecb6b3246a6854e5ad7a0b
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-06-21 11:06:17 +0200

    DONE!!

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 //Instructions
 // Using the usual Users and Todo lists:
-//   - Prompt the user for a letter. ✅
-//   - Find all the users who's name contains the letter. ✅
-//   - Say hi to those users in the console - but only 1 greeting every 2 seconds. ✅
-//   - Prompt the user for a user ID. Using the value, find all the todos that are Incomplete and console.log them.
+
+//   -
+//
+//
 // Tips
 // - Don't forget to clear interval once it's done running
 // - It's perfectly okay to prompt a user for details while other stuff (i.e.the interval) is running
 // // - Keep your code separated into functions for easier readability later
 
-let letter = prompt("Enter a letter") 
-let usersWithLetter = users.filter(user => user.name.toLowerCase().includes(letter.toLocaleLowerCase()))
+//   - Prompt the user for a letter. ✅
+let letter = prompt("Enter a letter");
 
+// Find all the users who's name contains the letter. ✅
+let usersWithLetter = users.filter((user) =>
+  user.name.toLowerCase().includes(letter.toLocaleLowerCase())
+);
 
-let i = 0
+// - Say hi to those users in the console - but only 1 greeting every 2 seconds. ✅
+let i = 0;
 let intervalId = setInterval(() => {
-  let user = usersWithLetter[i]
+  let user = usersWithLetter[i];
   if (user) {
-  console.log(`Hi ${user.name}`)
-  i++
-} else {
-  clearInterval(intervalId)
-}
-}, 2000)
+    console.log(`Hi ${user.name}`);
+    i++;
+  } else {
+    clearInterval(intervalId);
+  }
+}, 2000);
 
-let userId = prompt("Enter a user ID")
+// - Prompt the user for a user ID. ✅
+let userId = Number(prompt("Enter a user ID"));
+// Using the value, find all the todos that are Incomplete and console.log them. ✅
 let incompleteUserTodos = todos.filter(
-  todo => todo.userId === userId && todo.completed === false
-  )
-// let incompleteTodos = userTodos.filter(todo => todo.completed === false)
+  todo=> todo.userId === userId && todo.completed === false
+);
+console.log(incompleteUserTodos);
 
-  
-console.log(incompleteUserTodos)
-
-// console.log('Users with letter:', letter)
-// console.log(usersWithLetter)
